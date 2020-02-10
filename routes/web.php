33396aa7662d997d453b13f253c3a9772be36b73
pablo <pablo_jose12@hotmail.es>
2020-02-10 12:16:26 -0600

    creacion completa de logueo, auth de autenticacion de usuario - controlador y request de Usuario

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Route::resource('almacen/categoria','CategoriaController');
@@ -23,3 +23,8 @@ Route::resource('ventas/venta','VentaController');
 
 Route::resource('compras/proveedor','ProveedorController');
 Route::resource('compras/ingreso','IngresoController');
+
+Route::auth();
+Route::get('/logout','Auth\LoginController@logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
